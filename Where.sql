f7ccaf2657332211305cd1b90fd196855a836c80
italo <italogithub@gmail.com>
2023-04-15 17:26:01 -0300

    feat: adicionando lógica no banco de dados

--- Where.sql
+++ Where.sql
@@ -1,7 +1,30 @@
+# definição
+
 SELECT coluna1, coluna2, coluna_n
 FROM tabela
 WHERE condicao
 
+# Exemplos
+
+SELECT *
+FROM person.person
+WHERE LastName = 'Miller' and FirstName = 'anna';
+
+SELECT *
+FROM production.Product
+WHERE color = 'blue' or color = 'black';
+
+SELECT *
+FROM production.Product
+WHERE ListPrice > 1500 and ListPrice < 5000;
+
+SELECT *
+FROM production.Product
+WHERE color <> 'red' and color <> 'yellow';
+
+/*  
+    Nestes exemplos eu faço comparações no banco de dados, fazendo com que a minha filtragem seja mais dinâmica e perfomática.
+*/
 
 /* 
 OPERADOR                     DESCRIÇÃO
