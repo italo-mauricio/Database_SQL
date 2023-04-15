76eecb697ae7b8ce377f3c8a6467dedec6365a37
italo <italogithub@gmail.com>
2023-04-15 19:20:39 -0300

    add exercise 4

---
+++ Exercicios/Four.sql
@@ -0,0 +1,26 @@
+/*
+
+Desafio 1:
+
+Eu quero saber quantos produtos temos cadastrados em nossa tabela de produtos (production.product)
+
+*/
+
+#Resposta
+
+SELECT count(*)
+FROM Production.Product;
+
+
+/*
+
+Desafio 2:
+
+Eu quero saber quantos tamanhos de produtos temos cadastrados em nossa tabela (productions.product)
+
+*/
+
+#Resposta
+
+SELECT count(size)
+FROM Production.Product;
