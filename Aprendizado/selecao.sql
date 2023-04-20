f6aca232195984bbc22a4560a6c292b39b380178
italo <italogithub@gmail.com>
2023-04-20 10:12:04 -0300

    update

--- Aprendizado/selecao.sql
+++ Aprendizado/selecao.sql
@@ -26,3 +26,20 @@ Após o WHERE especificamos uma ou mais condições
     Todas as linhas, em que a condição resultar em verdadeiro serão devolvidas pelo SGBD.
 
     – As linhas, onde a condição resultar em falso, serão descartadas pelo SGBD, e não serão retornadas.
+
+
+
+Podemos combinar PROJEÇÃO E SELEÇÃO, separar matricula, nome e email dos registros/linhas/tuplas que são da cidade de RECIFE (por exemplo):
+
+código: 
+
+SELECT matricula, nome, email
+FROM aluno
+WHERE cidade = 'Recife'
+
+
+Output:     matricula            nome                email  
+           201.610.253      ANGELO GAWLINSKI        angelo@gmail.com 
+           201.618.375      CESAR AUGUSTO           cesar@gmail.com 
+
+Não vai aparecer a cidade "RECIFE" pois não especificamos a cidade no SELECT porém se você pedir pra ele demonstrar, vai verificar que a busca foi realizada com sucesso e esses dois nomes estão no banco de dados na cidade de Recife. 
