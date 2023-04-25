beb0e96cde4a62f39629d4888a415f5d2c080a29
italo <italogithub@gmail.com>
2023-04-24 23:40:12 -0300

    update

---
+++ Exercicios-Estagi/ExDML3.sql
@@ -0,0 +1,9 @@
+                                    Exercícios
+Dica 1: No jargão da informática, atualizar significa modificar dados da tabela. Que comando utilizar?
+Dica 2: Em cada questão, separe o que é PROJEÇÃO e o que é SELEÇÃO, para então montar o comando.
+
+
+1) Selecione todos os alunos que não possuem telefone e coloque em alguns a informação do telefone:
+
+
+Resp 
