68e040d9288dcb0e2b7e5190490cd83a10768346
italo <italogithub@gmail.com>
2023-04-24 22:59:56 -0300

    update by order

--- Aprendizado/OderBy.sql
+++ Aprendizado/OderBy.sql
@@ -18,3 +18,17 @@ SELECT *
 FROM person.Person
 ORDER BY FirstName asc ,LastName desc;
 
+●Classifica as linhas em ordem crescente ou decrescente;
+
+●Só pode haver um Order By por Select;
+
+SELECT [DISTINCT] {*,nomeColuna [as apelido]}
+FROM nomeTabela
+[WHERE condição(ções)]
+[ORDER BY {coluna, expr, ...} [ASC|DESC]];
+
+ORDER BY- especifica a ordem em que as linhas recuperadas são exibidas
+ASC
+- ordena as linhas na ordem crescente (default)
+DESC
+- ordena as linhas na ordem decrescente
---
+++ Aprendizado/Precedencia.sql
@@ -0,0 +1,13 @@
+                        Regras de Precedência
+
+
+ORDEM DE AVALIAÇÃO                  OPERADOR
+
+1                                 todos os operadores de comparação
+2                                 NOT
+3                                 AND
+4                                 OR
+●
+
+
+Regras de precedência podem ser reestruturadas usando parênteses!
