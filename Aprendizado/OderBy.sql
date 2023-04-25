f1739cf767b638e0af975d3d3087d70d3632bd90
italo <italogithub@gmail.com>
2023-04-24 23:04:42 -0300

    update

--- Aprendizado/OderBy.sql
+++ Aprendizado/OderBy.sql
@@ -32,3 +32,46 @@ ASC
 - ordena as linhas na ordem crescente (default)
 DESC
 - ordena as linhas na ordem decrescente
+
+
+Exemplo: 
+
+SELECT email,nome,matricula
+FROM aluno
+WHERE nome LIKE 'MAR%'
+AND idade >=18
+ORDER BY cidade ASC;
+
+SELECT *
+FROM aluno
+WHERE (cidade = 'PELOTAS' OR cidade = 'CRISTAL')
+AND idade > 20
+ORDER BY idade;
+
+SELECT nome
+FROM aluno
+WHERE nome LIKE '_A%'
+ORDER BY matricula DESC;
+
+Podemos usar o Apelido (ALIAS) da coluna como referência: 
+
+    SELECT telefone,nome, dtnascimento AS quandoNasceu
+    FROM aluno
+    WHERE telefone IS NOT NULL
+    ORDER BY quandoNasceu;
+
+Podemos usar o número da coluna que será retornada como referência:
+
+    SELECT cidade, telefone,nome,
+    FROM aluno
+    ORDER BY 2;
+
+● A cláusula ORDER BY permite várias colunas:
+
+    SELECT telefone,nome, dtnascimento, matricula
+    FROM aluno
+    ORDER BY nome,dtnascimento DESC;
+
+    SELECT telefone,nome, dtnascimento, matricula
+    FROM aluno
+    ORDER BY nome,dtnascimento ASC;
