f5028f1ed87be75ad3f37c1d7f2180901ef04b18
italo <italogithub@gmail.com>
2023-04-25 08:21:16 -0300

    add exercicios dml2

--- Exercicios-Estagi/ExDML3.sql
+++ Exercicios-Estagi/ExDML3.sql
@@ -6,4 +6,12 @@ Dica 2: Em cada questão, separe o que é PROJEÇÃO e o que é SELEÇÃO, para 
 1) Selecione todos os alunos que não possuem telefone e coloque em alguns a informação do telefone:
 
 
-Resp 
+Resp:   select * 
+        from aluno
+        where telefone is null;    verifico quais são null na tabela 
+
+        update aluno set telefone = '213232312'
+        where telefone is null;    caso eu queria colocar um telefone em todos que são null eu uso esta linha
+
+        update aluno set telefone = '111111'
+        where matricula = 188988   caso eu queira troar especificamente o numero de alguem 
