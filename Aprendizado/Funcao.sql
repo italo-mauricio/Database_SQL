64a5ea0b7a914e73f9d3986052e7cc6734cfd886
italo <italogithub@gmail.com>
2023-04-26 10:11:06 -0300

    update

--- Aprendizado/Funcao.sql
+++ Aprendizado/Funcao.sql
@@ -15,3 +15,29 @@
             ● http://www.postgresql.org/docs/9.3/interactive/index.html
             ● http://dev.mysql.com/doc/refman/5.0/en/functions.html
             ● http://www.oracle.com/technology/documentation/index.html
+
+
+- Dois tipos de funções SQL 
+
+1 - Funções de uma única linha: são funções que operam linha a linha 
+
+2 - Funções de grupo: são funções que recebem várias entradas e devolvem um único resultado 
+
+                    Onde é possível usar
+    
+    ● As funções de um modo geral podem ser utilizadas
+    – Na projeção e na seleção do comando Select
+        
+        SELECT funcao2(coluna1) AS APELIDO
+        FROM tabela
+        WHERE funcao3(coluna2) = valor1;
+
+    – Nas restrições dos comandos Update e ou Delete
+
+        UPDATE tabela
+        SET coluna4=valor1
+        WHERE funcao6(coluna3)=valor2;
+    
+    – Nos novos valores do comando Update e Insert
+
+        INSERT INTO tabela (coluna1,colu
