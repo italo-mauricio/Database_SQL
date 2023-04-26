f5eec9dbe010044b910d395eedaa7b390ba04ac8
italo <italogithub@gmail.com>
2023-04-26 09:54:03 -0300

    feat: add funções sql

---
+++ Aprendizado/Funcao.sql
@@ -0,0 +1,17 @@
+                         Funções SQL
+
+            ● Recurso avançado do SQL, serve para:
+            – Executar cálculos usando dados
+            – Modificar itens de dados individuais
+            – Manipular saída para grupos de linha
+            – Formatar datas e números para exibição
+            – Converter tipos de dados de colunas
+
+            ● Funções não são padronizadas pelo SQL.
+            – Cada Versão/Produto de SGBD tem as suas
+            ORACLE <> PostgreSQL <> JavaDB <> MySQL <> SQL Server <> ...
+
+            ● http://developers.sun.com/docs/javadb/10.4.1.3/ref/ref-single.html
+            ● http://www.postgresql.org/docs/9.3/interactive/index.html
+            ● http://dev.mysql.com/doc/refman/5.0/en/functions.html
+            ● http://www.oracle.com/technology/documentation/index.html
