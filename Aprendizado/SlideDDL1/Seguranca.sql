3dfe08650937aa35103b921309ae8b4b735df7bf
italo <italogithub@gmail.com>
2023-05-02 10:08:47 -0300

    update

---
+++ Aprendizado/SlideDDL1/Seguranca.sql
@@ -0,0 +1,14 @@
+                        Segurança <> Integridade
+
+
+● Segurança está relacionada a proteção dos dados contra o acesso
+não autorizado
+● Integridade está relacionada a proteção quanto a perda de
+consistência.
+● Garantia de segurança implica em Medidas de segurança que devem
+ser tomadas em diversos níveis:
+
+    – Físico
+    – Humano
+    – Sistema Operacional
+    – SGBD
