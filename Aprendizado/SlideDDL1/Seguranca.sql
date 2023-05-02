503f684d2be98f69535012119ccc29137b675626
italo <italogithub@gmail.com>
2023-05-02 10:11:32 -0300

    add: aula sobre segurança no SGBD

--- Aprendizado/SlideDDL1/Seguranca.sql
+++ Aprendizado/SlideDDL1/Seguranca.sql
@@ -12,3 +12,29 @@ ser tomadas em diversos níveis:
     – Humano
     – Sistema Operacional
     – SGBD
+
+
+                        Restrições de Integridade
+● Garantem que as mudanças feitas no banco de dados por usuários autorizados não
+resultam em uma perda de consistência dos dados. Portanto as restrições protegem
+contra danos acidentais no banco de dados.
+● Chave Primária (PK) e Chave Estrangeira (FK)
+● NOT NULL
+● UNIQUE
+● CHECK (CONDIÇÕES)
+
+CREATE TABLE FUNCIONARIO (
+    COD NUMERIC(8),
+    NOME VARCHAR(200) NOT NULL,
+    CARGO VARCHAR(200),
+    DTCONTRATACAO TIMESTAMP,
+    RG VARCHAR(10),
+    ATIVO CHAR(1) NOT NULL DEFAULT 'S',
+    CODGERENTE NUMERIC(8),
+    SALARIO NUMERIC(12,2),
+    CODDEPARTAMENTO NUMERIC (6),
+    PRIMARY KEY (COD),
+    FOREIGN KEY (CODDEPARTAMENTO) REFERENCES DEPARTAMENTO(COD),
+    FOREIGN KEY (CODGERENTE) REFERENCES FUNCIONARIO(COD),
+    UNIQUE (RG),
+    CHECK ( ATIVO = 'S' OR ATIVO = 'N') );
