683b6078c8dcd14f65066f58921b90d220316f4c
italo <italogithub@gmail.com>
2023-05-02 11:03:25 -0300

    feat: add sequencias

--- Aprendizado/SlideDDL1/Sequencias.sql
+++ Aprendizado/SlideDDL1/Sequencias.sql
@@ -18,3 +18,30 @@ INSERT INTO aluno (matricula,nome)
 VALUES (NEXTVAL('seqAluno'),'BEBETO');
 
 -> seta1 select NEXTVAL('seqAluno');
+
+dblucianomertins=> CREATE SEQUENCE seqAluno;
+CREATE SEQUENCE
+dblucianomertins=> select NEXTVAL('seqAluno');      CREATE SEQUENCE seqDepartamento;
+nextval
+---------
+1
+(1 row)
+dblucianomertins=> select NEXTVAL('seqAluno');      CREATE TABLE departamento(
+                                                        cod numeric(6) default nextval('seqDepartamento'),
+                                                        descricao varchar(200),
+                                                        sigla varchar(100),
+                                                        PRIMARY KEY (cod));
+nextval
+---------
+2
+(1 row)
+dblucianomertins=> select NEXTVAL('seqAluno');      
+nextval
+---------                                           
+3
+(1 row)
+dblucianomertins=>                                    
+
+INSERT INTO departamento (descricao,sigla) VALUES ('VENDAS','VND');
+
+
