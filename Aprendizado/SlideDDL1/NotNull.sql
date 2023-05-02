f347a0ba926d4546d7b00d523d1f519714337535
italo <italogithub@gmail.com>
2023-05-02 10:34:46 -0300

    formating

--- Aprendizado/SlideDDL1/NotNull.sql
+++ Aprendizado/SlideDDL1/NotNull.sql
@@ -14,19 +14,19 @@ A seguir, alguns exemplos de uso da restrição NOT NULL em um banco de dados:
     Em uma tabela de notas de alunos, a coluna "Nota" pode ser definida como NOT NULL. Isso garante que todas as notas tenham um valor válido registrado, evitando que notas nulas ou vazias sejam inseridas na tabela.
 
     Em uma tabela de produtos, a coluna "Preço" pode ser definida como NOT NULL. Isso garante que todos os produtos tenham um preço válido registrado, evitando que registros inválidos sejam inseridos na tabela.
-    
+
 CREATE TABLE FUNCIONARIO (
-COD NUMERIC(8),
-NOME VARCHAR(200) NOT NULL,
-CARGO VARCHAR(200),
-DTCONTRATACAO TIMESTAMP,
-RG VARCHAR(10),
-ATIVO CHAR(1) NOT NULL DEFAULT 'S',
-CODGERENTE NUMERIC(8),
-SALARIO NUMERIC(12,2),
-CODDEPARTAMENTO NUMERIC (6),
-PRIMARY KEY (COD),
-FOREIGN KEY (CODDEPARTAMENTO) REFERENCES DEPARTAMENTO(COD),
-FOREIGN KEY (CODGERENTE) REFERENCES FUNCIONARIO(COD),
-UNIQUE (RG),
-CHECK ( ATIVO = 'S' OR ATIVO = 'N') );
+    COD NUMERIC(8),
+    NOME VARCHAR(200) NOT NULL,
+    CARGO VARCHAR(200),
+    DTCONTRATACAO TIMESTAMP,
+    RG VARCHAR(10),
+    ATIVO CHAR(1) NOT NULL DEFAULT 'S',
+    CODGERENTE NUMERIC(8),
+    SALARIO NUMERIC(12,2),
+    CODDEPARTAMENTO NUMERIC (6),
+    PRIMARY KEY (COD),
+    FOREIGN KEY (CODDEPARTAMENTO) REFERENCES DEPARTAMENTO(COD),
+    FOREIGN KEY (CODGERENTE) REFERENCES FUNCIONARIO(COD),
+    UNIQUE (RG),
+    CHECK ( ATIVO = 'S' OR ATIVO = 'N') );
