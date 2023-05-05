89e3d184c9b09a36d1b91803902124af21c27dce
italo <italogithub@gmail.com>
2023-05-05 12:37:47 -0300

    update

---
+++ Aprendizado/SlideDML6/ConsultaSubconsulta.sql
@@ -0,0 +1,11 @@
+            Consultas Aninhadas (Subconsultas)
+
+
+● Uma subconsulta é uma instrução SELECT adicionada
+dentro de uma instrução SELECT, INSERT, DELETE,
+ou UPDATE ou dentro de outra subconsulta.
+
+● Precisamos de subconsultas, para resolver problemas
+mais complexos, que não são atendidos pelos
+mecanismos vistos até o momento.
+
