fb719d3495224a592e3a4c3625cdb879993b2dc6
italo <italogithub@gmail.com>
2023-05-08 10:12:14 -0300

    update

--- Exercicios-Estagi/ExDDL2.sql
+++ Exercicios-Estagi/ExDDL2.sql
@@ -3,4 +3,31 @@
 
 Resp:
 
+    CREATE TABLE faixaNivel1 (
+    idade_min INT,
+    idade_max INT,
+    nivel CHAR(1)
+    );
+
+    Aqui estou criando a tabela faixa nivel.
+
+
+    INSERT INTO faixaNivel1 (idade_min, idade_max, nivel) VALUES
+    (15, 20, 'A'),
+    (21, 25, 'B'),
+    (26, 30, 'C'),
+    (31, NULL, 'D');
     
+    Aqui estou inserindo os dados na tablea, já com as descrições
+
+    CREATE VIEW alunos_nivel AS
+    SELECT a.matricula, a.nome, f.nivel
+    FROM aluno a
+    JOIN faixaNivel1 f ON a.idade BETWEEN f.idade_min AND COALESCE(f.idade_max, a.idade)
+
+    Aqui estou criando a view baseado em outra tabela gerada anteriormente (aluno).
+
+    select *
+    from aluno_nivel 
+
+    Aqui estou consultando o resultado.
