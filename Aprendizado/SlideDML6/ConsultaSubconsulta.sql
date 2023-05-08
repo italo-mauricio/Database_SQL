4d3a900433bb3c35e51c6683899c3ca6f0c881c8
italo <italogithub@gmail.com>
2023-05-07 21:07:34 -0300

    add information of subconsult

--- Aprendizado/SlideDML6/ConsultaSubconsulta.sql
+++ Aprendizado/SlideDML6/ConsultaSubconsulta.sql
@@ -9,9 +9,28 @@ ou UPDATE ou dentro de outra subconsulta.
 mais complexos, que não são atendidos pelos
 mecanismos vistos até o momento.
 
-- 
 
--
--
--
+                     Problema
+●  Como obter os funcionários que recebem salário acima da média salarial dos funcionários?
+
+É necessário duas consultas. Uma para calcular o salário
+médio de todos os funcionários e outra para descobrir quem
+recebe mais que o valor da primeira consulta.
+
+Isto pode ser feito manualmente em duas execuções de consulta ao
+SGBD.
+● Ou em uma única consulta, que use subconsulta.
+
+                    Comportamento
+
+● A subconsulta é executada uma vez antes da consulta
+principal
+● O resultado da subconsulta é usado pela consulta
+principal
+● Pode existir subconsulta da subconsulta
+● As subconsultas devem estar entre parênteses e estar
+no lado direito do operador de comparação
+● Subconsultas não podem possuir claúsula ORDER BY
+
+
 
